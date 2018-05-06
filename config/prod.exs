f41e4a30689e729d426c55d8c7ebf78d21db7711
Francisco Mangas <francisco.mangas.79@gmail.com>
2018-05-06 17:56:39 +0200

    Try Heroku Deploy again.

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,11 @@
 use Mix.Config
 
 config :blog, Blog.Endpoint,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "phoenix-blog-kata.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+       load_from_system_env: true,
+       url: [scheme: "https", host: "phoenix-blog-kata.herokuapp.com", port: 443],
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+       cache_static_manifest: "priv/static/cache_manifest.json",
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :logger, level: :info
 
@@ -14,5 +14,3 @@ config :elixir_test_app, Blog.Repo,
        url: System.get_env("DATABASE_URL"),
        pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
        ssl: true
-
-#import_config "prod.secret.exs"
